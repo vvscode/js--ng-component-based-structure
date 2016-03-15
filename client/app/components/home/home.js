c2d9fd5b5c2626009fe216c0e01a9e75fcc28d2b
Vasil Vanchuk <v.vanchuk@tut.by>
2016-03-15 18:52:31 +0300

    Fix step-2

--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -1,19 +1,21 @@
-// TODO: import required modules
+import './home.styl';
+import angular from 'angular';
+import _ from 'lodash';
 
-const home = angular.module('home', [
+// placing an export in front of ar var is the same
+// as exporting the var at the end of the file
+// using export {varname}
+export const home = angular.module('home', [])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
 
-])
-.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
-
-  $stateProvider.state('home', {
-    url: '/',
-    template: `
+    $stateProvider.state('home', {
+      url: '/',
+      template: `
       <section class="home">
         <div class="title">
           <h1>{{ title }}</h1>
         </div>
-
         <div class="items">
           <ul>
             <li ng-repeat="item in items">
@@ -23,15 +25,10 @@ const home = angular.module('home', [
         </div>
       </section>
     `,
-    controller: 'HomeController'
+      controller: 'HomeController'
+    });
+  })
+  .controller('HomeController', function($scope) {
+    $scope.title = 'Welcome to the blog!';
+    $scope.items = _.times(5, i => `I am item ${i}`);
   });
-})
-.controller('HomeController', function($scope) {
-  $scope.title = 'Welcome to the blog!'
-  // TODO: be sure to import lodash!
-  $scope.items = _.times(5, i => {
-    return `I am item ${i}`;
-  });
-});
-
-// TODO: export the module
