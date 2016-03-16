276d362c920bc1783672a572e13239fb133d83e8
Vasil Vanchuk <v.vanchuk@tut.by>
2016-03-16 20:15:33 +0300

    Step-3

--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -1,34 +1,16 @@
-import './home.styl';
 import angular from 'angular';
-import _ from 'lodash';
+import {homeDirective} from './home.directive';
 
 // placing an export in front of ar var is the same
 // as exporting the var at the end of the file
 // using export {varname}
 export const home = angular.module('home', [])
-  .config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
 
-    $stateProvider.state('home', {
-      url: '/',
-      template: `
-      <section class="home">
-        <div class="title">
-          <h1>{{ title }}</h1>
-        </div>
-        <div class="items">
-          <ul>
-            <li ng-repeat="item in items">
-              {{ item }}
-            </li>
-          </ul>
-        </div>
-      </section>
-    `,
-      controller: 'HomeController'
-    });
-  })
-  .controller('HomeController', function($scope) {
-    $scope.title = 'Welcome to the blog!';
-    $scope.items = _.times(5, i => `I am item ${i}`);
+  $stateProvider.state('home', {
+    url: '/',
+    template: '<home></home>'
   });
+})
+.directive('home', homeDirective);
